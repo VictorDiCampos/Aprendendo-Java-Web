61ce6a8af1370c1549e2a7b039ef2be913a0ba91
Victor Campos <victordicampos@gmail.com>
2015-10-21 22:51:01 -0200

    Primeiro commit JavaEE

---
+++ src/main/java/br/com/victordecampos/App.java
@@ -0,0 +1,13 @@
+package br.com.victordecampos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
